9c3e3dd4c19fc3afd927da1cfdf6ab9629799529
Michael Lawrie <michael@michaellawrie.com>
2022-03-08 17:22:18 -0600

    Switches to double quotes

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -17,18 +17,18 @@ const SummarySection = ({summary, subtitle}) => (
 )
 export const Sidebar = () => (
   <>
-    <SummarySection summary='⚡️ 1.4kW' subtitle='Power draw'/>
-    <SummarySection summary='☀️️ 5.8kW' subtitle='Solar power production'/>
-    <SummarySection summary='🔌️ 4.4kW' subtitle='Fed into grid'/>
+    <SummarySection summary="⚡️ 1.4kW" subtitle="Power draw"/>
+    <SummarySection summary="☀️️ 5.8kW" subtitle="Solar power production"/>
+    <SummarySection summary="🔌️ 4.4kW" subtitle="Fed into grid"/>
 
     <section className="h5 darkgray mb2">
       <h4 className="h4 mb1">Your devices:</h4>
-      <DeviceSection title='Air conditioner' usage='0.3093kW'/>
-      <DeviceSection title='Wi-Fi router' usage='0.0033kW'/>
-      <DeviceSection title='Humidifer' usage='0.0518kW'/>
-      <DeviceSection title='Smart TV' usage='0.1276kW'/>
-      <DeviceSection title='Diffuser' usage='0.0078kW'/>
-      <DeviceSection title='Refrigerator' usage='0.0923kW'/>
+      <DeviceSection title="Air conditioner" usage="0.3093kW"/>
+      <DeviceSection title="Wi-Fi router" usage="0.0033kW"/>
+      <DeviceSection title="Humidifer" usage="0.0518kW"/>
+      <DeviceSection title="Smart TV" usage="0.1276kW"/>
+      <DeviceSection title="Diffuser" usage="0.0078kW"/>
+      <DeviceSection title="Refrigerator" usage="0.0923kW"/>
     </section>
   </>
 )
